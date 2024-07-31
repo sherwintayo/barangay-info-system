2cf906dd9e4525f305f409d8f6ca91760a8a29a7
four-J-MAX <cuevaj186@gmail.com>
2024-07-31 21:24:38 +0800

    Update permit.php

--- pages/report/permit.php
+++ pages/report/permit.php
@@ -64,8 +64,13 @@
                     </table>
                 </div>
 
-                <a href="print.php?from_date=<?= $_GET['from_date'] ?>&to_date=<?= $_GET['to_date'] ?>&permit_filter" class="btn btn-primary btn-sm" style="margin-left: 20px;">Print</a>
-
+               <?php 
+                if (isset($_GET['from_date'])) {
+                    ?>
+                     <a href="print.php?from_date=<?= $_GET['from_date'] ?>&to_date=<?= $_GET['to_date'] ?>&permit_filter" class="btn btn-primary btn-sm" style="margin-left: 20px;">Print</a>
+                    <?php 
+                }
+               ?>
                 <script>
                     let btnPermit =document.getElementById("btnPermit")
 
