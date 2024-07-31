5e829e6f1fbf63e4253921556801c27c386e0eac
four-J-MAX <cuevaj186@gmail.com>
2024-07-31 21:25:29 +0800

    Update blotter.php

--- pages/report/blotter.php
+++ pages/report/blotter.php
@@ -62,6 +62,14 @@
         </tbody>
     </table>
 </div>
+
+  <?php 
+                if (isset($_GET['from_date'])) {
+                    ?>
+                     <a href="print.php?from_date=<?= $_GET['from_date'] ?>&to_date=<?= $_GET['to_date'] ?>&permit_filter" class="btn btn-primary btn-sm" style="margin-left: 20px;">Print</a>
+                    <?php 
+                }
+               ?>
 <script>
     function dateformat(date) {
         var d = new Date(date);
