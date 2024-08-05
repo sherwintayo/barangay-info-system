452a696a1d6525c93d7fcc2e91f26df3709506ff
four-J-MAX <cuevaj186@gmail.com>
2024-08-05 17:12:48 +0800

    Update login.php

--- login.php
+++ login.php
@@ -29,18 +29,19 @@ session_start();
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
 
     <style>
-  body{
-            background: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(./images/bg-img.jpeg);
+        body {
+            background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(./images/bg-img.jpeg);
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
         }
 
-        .container{
+        .container {
             height: 100vh;
             display: flex;
             align-items: center;
         }
+
         .eye-icon {
             position: absolute;
             top: 50%;
@@ -55,20 +56,16 @@ session_start();
         }
     </style>
 </head>
-<?php include 'pages/connection.php';
-$squery = mysqli_query($con, "SELECT * FROM tblsettings");
-$data = $squery->fetch_assoc();
-?>
 
 <body class="skin-black">
-    <div class="container" >
+    <div class="container">
         <div class="col-md-4 col-md-offset-4">
             <div class="panel panel-default">
                 <div class="panel-heading" style="text-align:center; ">
-                    <img src="images/<?= $data['logo'] ?>" alt="image" style="width: 200px;margin: 10px 0;">
+                    <img src="img/tugas_logo.png" style="height:150px;" />
                     <h3 class="panel-title">
                         <strong>
-                            <?php echo $data['name'] ?>
+                            Management System
                         </strong>
                     </h3>
                     <p>Login</p>
@@ -77,21 +74,20 @@ $data = $squery->fetch_assoc();
                     <form role="form" method="post">
                         <div class="form-group">
                             <label for="txt_username">Username</label>
-                            <input type="text" class="form-control" style="border-radius:0px" name="txt_username"
-                                placeholder="Enter Username" required="" />
+                            <input type="text" class="form-control" style="border-radius:0px" name="txt_username" placeholder="Enter Username" required="" />
                         </div>
                         <div class="form-group">
                             <label for="txt_password">Password</label>
                             <div style="position: relative;">
-                                <input type="password" class="form-control" style="border-radius:0px"
-                                    name="txt_password" id="txt_password" placeholder="Enter Password" required="" />
+                                <input type="password" class="form-control" style="border-radius:0px" name="txt_password" id="txt_password" placeholder="Enter Password" required="" />
                                 <i class="fas fa-eye eye-icon" id="togglePassword"></i>
                             </div>
                         </div>
-                        <button type="submit" class="btn btn-sm btn-primary" name="btn_login"
-                            style="background-color:#00BB27;">Log in</button>
+                        <button type="submit" class="btn btn-sm btn-primary" name="btn_login" style="background-color:#00BB27;">Log in</button>
                         <a href="pages/resetpassword.php" style="float: right;">Forgot password</a>
                         <label id="error" class="label label-danger pull-right"></label>
+
+                        <p class="text-center" style="margin-top: 20px;">Don't have an account? <a href="signup.php">Sign Up</a></p>
                     </form>
                 </div>
             </div>
@@ -106,113 +102,190 @@ $data = $squery->fetch_assoc();
         $username = $_POST['txt_username'];
         $password = $_POST['txt_password'];
 
-        // Prepare and bind parameters
-        $stmt = $con->prepare("SELECT * FROM tbluser WHERE username = ? AND password = ? AND type = 'administrator'");
+        $stmt = $con->prepare("SELECT * FROM tbluser WHERE username = ? AND password = ?");
         $stmt->bind_param("ss", $username, $password);
-        $stmt->execute();
-        $admin_result = $stmt->get_result();
+        if ($stmt->execute()) {
+            $result = $stmt->get_result();
+            if ($result->num_rows > 0) {
+                
+                $row = $result->fetch_assoc();
 
-        $stmt = $con->prepare("SELECT * FROM tblzone WHERE username = ? AND password = ?");
-        $stmt->bind_param("ss", $username, $password);
-        $stmt->execute();
-        $zone_result = $stmt->get_result();
+                if ($row['type'] == 'administrator') {
+                    $_SESSION['role'] = "Administrator";
+                    $_SESSION['userid'] = $row['id'];
+                    $_SESSION['username'] = $row['username'];
 
-        $stmt = $con->prepare("SELECT * FROM tblstaff WHERE username = ? AND password = ?");
-        $stmt->bind_param("ss", $username, $password);
-        $stmt->execute();
-        $staff_result = $stmt->get_result();
+                    echo "<script>
+                                Swal.fire({
+                                    title: 'Success!',
+                                    text: 'Welcome, Administrator!',
+                                    icon: 'success',
+                                    timer: 2000,
+                                    showConfirmButton: false
+                                }).then(() => {
+                                    window.location.href = 'pages/dashboard/dashboard.php';
+                                });
+                            </script>
+                        ";
+                } else if ($row['type'] == 'Zone Leader') {
+                    $_SESSION['role'] = "Zone Leader";
+                    $_SESSION['userid'] = $row['id'];
+                    $_SESSION['username'] = $row['username'];
+                    $_SESSION['barangay'] = $row['barangay'];
 
-        $stmt = $con->prepare("SELECT * FROM tblresident WHERE username = ? AND password = ?");
-        $stmt->bind_param("ss", $username, $password);
-        $stmt->execute();
-        $user_result = $stmt->get_result();
+                    echo "<script>
+                                Swal.fire({
+                                    title: 'Success!',
+                                    text: 'Welcome, Zone Leader!',
+                                    icon: 'success',
+                                    timer: 2000,
+                                    showConfirmButton: false
+                                }).then(() => {
+                                    window.location.href = 'pages/permit/permit.php';
+                                });
+                            </script>
+                        ";
 
-        if ($admin_result->num_rows > 0) {
-            while ($row = $admin_result->fetch_assoc()) {
-                $_SESSION['role'] = "Administrator";
-                $_SESSION['userid'] = $row['id'];
-                $_SESSION['username'] = $row['username'];
+                }
+            } else {
+                    echo "<script>
+                    Swal.fire({
+                        title: 'Error!',
+                        text: 'Invalid username or password.',
+                        icon: 'error',
+                        timer: 2000,
+                        showConfirmButton: false
+                    });
+                </script>";
             }
-
-
-            echo "<script>
-            Swal.fire({
-                title: 'Success!',
-                text: 'Welcome, Administrator!',
-                icon: 'success',
-                timer: 2000,
-                showConfirmButton: false
-            }).then(() => {
-                window.location.href = 'pages/dashboard/dashboard.php';
-            });
-        </script>";
-        } elseif ($zone_result->num_rows > 0) {
-            while ($row = $zone_result->fetch_assoc()) {
-                $_SESSION['role'] = "Zone Leader";
-                $_SESSION['userid'] = $row['id'];
-                $_SESSION['username'] = $row['username'];
-            }
-            echo "<script>
-            Swal.fire({
-                title: 'Success!',
-                text: 'Welcome, Zone Leader!',
-                icon: 'success',
-                timer: 2000,
-                showConfirmButton: false
-            }).then(() => {
-                window.location.href = 'pages/permit/permit.php';
-            });
-        </script>";
-        } elseif ($staff_result->num_rows > 0) {
-            while ($row = $staff_result->fetch_assoc()) {
-                $_SESSION['role'] = $row['name'];
-                $_SESSION['staff'] = "Staff";
-                $_SESSION['userid'] = $row['id'];
-                $_SESSION['username'] = $row['username'];
-            }
-            echo "<script>
-            Swal.fire({
-                title: 'Success!',
-                text: 'Welcome, Staff Member!',
-                icon: 'success',
-                timer: 2000,
-                showConfirmButton: false
-            }).then(() => {
-                window.location.href = 'pages/resident/resident.php';
-            });
-        </script>";
-        } elseif ($user_result->num_rows > 0) {
-            while ($row = $user_result->fetch_assoc()) {
-                $_SESSION['role'] = $row['fname'];
-                $_SESSION['resident'] = 1;
-                $_SESSION['userid'] = $row['id'];
-                $_SESSION['username'] = $row['username'];
-            }
-            echo "<script>
-            Swal.fire({
-                title: 'Success!',
-                text: 'Welcome, Resident!',
-                icon: 'success',
-                timer: 2000,
-                showConfirmButton: false
-            }).then(() => {
-                window.location.href = 'pages/permit/permit.php';
-            });
-        </script>";
-        } else {
-            echo "<script>
-            Swal.fire({
-                title: 'Error!',
-                text: 'Invalid username or password.',
-                icon: 'error',
-                timer: 2000,
-                showConfirmButton: false
-            });
-        </script>";
         }
+
+
+        // Prepare and bind parameters
+        // $stmt = $con->prepare("SELECT * FROM tbluser WHERE username = ? AND password = ? AND type = 'administrator'");
+        // $stmt->bind_param("ss", $username, $password);
+        // $stmt->execute();
+        // $admin_result = $stmt->get_result();
+
+        // $stmt = $con->prepare("SELECT * FROM tblzone WHERE username = ? AND password = ?");
+        // $stmt->bind_param("ss", $username, $password);
+        // $stmt->execute();
+        // $zone_result = $stmt->get_result();
+
+        // $stmt = $con->prepare("SELECT * FROM tblstaff WHERE username = ? AND password = ?");
+        // $stmt->bind_param("ss", $username, $password);
+        // $stmt->execute();
+        // $staff_result = $stmt->get_result();
+
+        // $stmt = $con->prepare("SELECT * FROM tblresident WHERE username = ? AND password = ?");
+        // $stmt->bind_param("ss", $username, $password);
+        // $stmt->execute();
+        // $user_result = $stmt->get_result();
+
+        // if ($admin_result->num_rows > 0) {
+        //     while ($row = $admin_result->fetch_assoc()) {
+        //         $_SESSION['role'] = "Administrator";
+        //         $_SESSION['userid'] = $row['id'];
+        //         $_SESSION['username'] = $row['username'];
+        //     }
+        //     echo "<script>
+        //     Swal.fire({
+        //         title: 'Success!',
+        //         text: 'Welcome, Administrator!',
+        //         icon: 'success',
+        //         timer: 2000,
+        //         showConfirmButton: false
+        //     }).then(() => {
+        //         window.location.href = 'pages/dashboard/dashboard.php';
+        //     });
+        // </script>";
+        // } elseif ($zone_result->num_rows > 0) {
+        //     while ($row = $zone_result->fetch_assoc()) {
+        //         $_SESSION['role'] = "Zone Leader";
+        //         $_SESSION['userid'] = $row['id'];
+        //         $_SESSION['username'] = $row['username'];
+        //     }
+        //     echo "<script>
+        //     Swal.fire({
+        //         title: 'Success!',
+        //         text: 'Welcome, Zone Leader!',
+        //         icon: 'success',
+        //         timer: 2000,
+        //         showConfirmButton: false
+        //     }).then(() => {
+        //         window.location.href = 'pages/permit/permit.php';
+        //     });
+        // </script>";
+        // } elseif ($staff_result->num_rows > 0) {
+        //     while ($row = $staff_result->fetch_assoc()) {
+        //         $_SESSION['role'] = $row['name'];
+        //         $_SESSION['staff'] = "Staff";
+        //         $_SESSION['userid'] = $row['id'];
+        //         $_SESSION['username'] = $row['username'];
+        //     }
+        //     echo "<script>
+        //     Swal.fire({
+        //         title: 'Success!',
+        //         text: 'Welcome, Staff Member!',
+        //         icon: 'success',
+        //         timer: 2000,
+        //         showConfirmButton: false
+        //     }).then(() => {
+        //         window.location.href = 'pages/resident/resident.php';
+        //     });
+        // </script>";
+        // } elseif ($user_result->num_rows > 0) {
+        //     while ($row = $user_result->fetch_assoc()) {
+        //         $_SESSION['role'] = $row['fname'];
+        //         $_SESSION['resident'] = 1;
+        //         $_SESSION['userid'] = $row['id'];
+        //         $_SESSION['username'] = $row['username'];
+        //     }
+        //     echo "<script>
+        //     Swal.fire({
+        //         title: 'Success!',
+        //         text: 'Welcome, Resident!',
+        //         icon: 'success',
+        //         timer: 2000,
+        //         showConfirmButton: false
+        //     }).then(() => {
+        //         window.location.href = 'pages/permit/permit.php';
+        //     });
+        // </script>";
+        // } else {
+        //     echo "<script>
+        //     Swal.fire({
+        //         title: 'Error!',
+        //         text: 'Invalid username or password.',
+        //         icon: 'error',
+        //         timer: 2000,
+        //         showConfirmButton: false
+        //     });
+        // </script>";
+        // }
+
+
+
     }
     ?>
 
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            let passwordInp = document.querySelector("input[name='txt_password']");
+            let showPass = document.getElementById("togglePassword");
+
+            showPass.onclick = () => {
+                if (passwordInp.getAttribute("type") == "password") {
+                    passwordInp.setAttribute("type", "text")
+                    showPass.classList.replace("fa-eye", "fa-eye-slash")
+                } else {
+                    passwordInp.setAttribute("type", "password")
+                    showPass.classList.replace("fa-eye-slash", "fa-eye")
+                }
+            }
+
+        })
+    </script>
 </body>
 
 </html>
