e5af3ef4cca9857f2a37d997260c3a3064d7a567
four-J-MAX <cuevaj186@gmail.com>
2024-08-08 09:41:25 +0800

    Update ZoneLeader.php

--- pages/permit/ZoneLeader.php
+++ pages/permit/ZoneLeader.php
@@ -1,59 +1,47 @@
 <div class="row">
-                            <!-- left column -->
-                            <div class="box">
-                                <div class="box-header">
-
-                                </div><!-- /.box-header -->
-                                <div class="box-body table-responsive">
-
-                                    <form method="post">
-
-
-                                        <table id="table" class="table table-bordered table-striped">
-                                            <thead>
-                                                <tr>
-                                                    <th style="width: 20px !important;"><input type="checkbox"
-                                                            name="chk_delete[]" class="cbxMain" onchange="checkMain(this)" />
-                                                    </th>
-                                                    <th>Resident</th>
-                                                    <th>Business Name</th>
-                                                    <th>Business Address</th>
-
-                                                    <th style="width: 25% !important;">Option</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <?php
-                                                $squery = mysqli_query($con, "SELECT *,CONCAT(r.lname, ', ' ,r.fname, ' ' ,r.mname) as residentname,p.id as pid FROM tblpermit p left join tblresident r on r.id = p.residentid where status = 'New'") or die('Error: ' . mysqli_error($con));
-                                                while ($row = mysqli_fetch_array($squery)) {
-                                                    echo '
-                                                <tr>
-                                                    <td><input type="checkbox" name="chk_delete[]" class="chk_delete" value="' . $row['pid'] . '" /></td>
-                                                    <td>' . $row['residentname'] . '</td>
-                                                    <td>' . $row['businessName'] . '</td>
-                                                    <td>' . $row['businessAddress'] . '</td>
-                                                    
-                                                    <td>
-                                                        <button class="btn btn-success btn-sm" data-target="#approveModal' . $row['pid'] . '" data-toggle="modal"><i class="fa fa-thumbs-up" aria-hidden="true"></i> Approve</button>
-                                                        <button class="btn btn-danger btn-sm" data-target="#disapproveModal' . $row['pid'] . '" data-toggle="modal"><i class="fa fa-thumbs-down" aria-hidden="true"></i> Disapprove</button>
-                                                    </td>
-                                                </tr>
-                                                ';
-                                                    include "approve_modal.php";
-                                                    include "disapprove_modal.php";
-                                                }
-                                                ?>
-                                            </tbody>
-                                        </table>
-
-
-                                        <?php include "../deleteModal.php"; ?>
-
-                                    </form>
-                                </div><!-- /.box-body -->
-                            </div><!-- /.box -->
-
-                            <?php include "function.php"; ?>
-
-
-                        </div> <!-- /.row -->
+    <!-- left column -->
+    <div class="box">
+        <div class="box-header">
+        </div><!-- /.box-header -->
+        <div class="box-body table-responsive">
+            <form method="post">
+                <table id="table" class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th style="width: 20px !important;"><input type="checkbox"
+                                    name="chk_delete[]" class="cbxMain" onchange="checkMain(this)" />
+                            </th>
+                            <th>Resident</th>
+                            <th>Business Name</th>
+                            <th>Business Address</th>
+                            <th style="width: 25% !important;">Option</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $squery = mysqli_query($con, "SELECT *, CONCAT(r.lname, ', ' ,r.fname, ' ' ,r.mname) as residentname, p.id as pid FROM tblpermit p LEFT JOIN tblresident r ON r.id = p.residentid WHERE p.status = 'New'") or die('Error: ' . mysqli_error($con));
+                        while ($row = mysqli_fetch_array($squery)) {
+                            echo '
+                        <tr>
+                            <td><input type="checkbox" name="chk_delete[]" class="chk_delete" value="' . $row['pid'] . '" /></td>
+                            <td>' . $row['residentname'] . '</td>
+                            <td>' . $row['businessName'] . '</td>
+                            <td>' . $row['businessAddress'] . '</td>
+                            <td>
+                                <button class="btn btn-success btn-sm" data-target="#approveModal' . $row['pid'] . '" data-toggle="modal"><i class="fa fa-thumbs-up" aria-hidden="true"></i> Approve</button>
+                                <button class="btn btn-danger btn-sm" data-target="#disapproveModal' . $row['pid'] . '" data-toggle="modal"><i class="fa fa-thumbs-down" aria-hidden="true"></i> Disapprove</button>
+                            </td>
+                        </tr>
+                        ';
+                            include "approve_modal.php";
+                            include "disapprove_modal.php";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+                <?php include "../deleteModal.php"; ?>
+            </form>
+        </div><!-- /.box-body -->
+    </div><!-- /.box -->
+    <?php include "function.php"; ?>
+</div> <!-- /.row -->
