87a7129bb75d3c43a4247613ca82351049c9b817
four-J-MAX <cuevaj186@gmail.com>
2024-08-13 12:31:47 +0800

    Update newResident.php

--- pages/resident/newResident.php
+++ pages/resident/newResident.php
@@ -65,7 +65,11 @@ if (!isset($_SESSION['role'])) {
                                         <span class="info-box-text">Total Resident</span>
                                         <span class="info-box-number" style="text-align: center;">
                                             <?php
+                                           if ($isZoneLeader) {
+                                            $q = mysqli_query($con, "SELECT * from tblresident where statRes=0 AND status = 'New Resident' AND barangay = '$zone_barangay'");
+                                           }else{
                                             $q = mysqli_query($con, "SELECT * from tblresident where statRes=0 AND status = 'New Resident'");
+                                           }
                                             $num_rows = mysqli_num_rows($q);
                                             echo $num_rows;
                                             ?>
@@ -130,7 +134,14 @@ if (!isset($_SESSION['role'])) {
                                             <tbody>
                                                 <?php
                                                 if (!isset($_SESSION['staff'])) {
-                                                    $squery = mysqli_query($con, "SELECT *,zone,id,CONCAT(lname, ', ', fname, ' ', mname) as cname, age, gender, formerAddress, image FROM tblresident where statRes=0 AND status = 'New Resident' order by zone ");
+
+                                                    if ($isZoneLeader) {
+                                                        $squery = mysqli_query($con, "SELECT *,zone,id,CONCAT(lname, ', ', fname, ' ', mname) as cname, age, gender, formerAddress, image FROM tblresident where statRes=0 AND status = 'New Resident' AND barangay = '$zone_barangay' order by zone ");
+                                                    }else{
+                                                        $squery = mysqli_query($con, "SELECT *,zone,id,CONCAT(lname, ', ', fname, ' ', mname) as cname, age, gender, formerAddress, image FROM tblresident where statRes=0 AND status = 'New Resident' order by zone ");
+                                                    }
+
+                                                   
                                                     while ($row = mysqli_fetch_array($squery)) {
                                                         echo '
                                                     <tr>
