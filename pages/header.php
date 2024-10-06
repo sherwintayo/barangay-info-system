7ae70cf25734121b504e6bea69bdb9fba62821ae
four-J-MAX <cuevaj186@gmail.com>
2024-10-05 22:28:37 -0700

    commit

--- pages/header.php
+++ pages/header.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html>
-<html>
+
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
