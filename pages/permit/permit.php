0157bf5e60796b7de88abecceeaa310717ecafe7
four-J-MAX <cuevaj186@gmail.com>
2024-10-05 22:59:49 -0700

    commit

--- pages/permit/permit.php
+++ pages/permit/permit.php
@@ -9,9 +9,19 @@ if (!isset($_SESSION['role'])) {
 } else {
     ob_start();
     include('../head_css.php');
+    
 
 
 ?>
+
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+<script src="./assets/js/jquery.min.js"></script>
+<script src="./assets/js/bootstrap.min.js"></script>
+
+</head>
 
     <body class="skin-black">
         <!-- header logo: style can be found in header.less -->
