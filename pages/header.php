56e289b19625493028306926066e9da068decf76
four-J-MAX <cuevaj186@gmail.com>
2024-10-05 22:50:29 -0700

    commit

--- pages/header.php
+++ pages/header.php
@@ -3,3 +3,40 @@
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
 <script src="./assets/js/jquery.min.js"></script>
 <script src="./assets/js/bootstrap.min.js"></script>
+
+<?php 
+    function clean($data){
+        $data = htmlspecialchars(stripslashes(trim($data)));
+        return $data;
+    }
+
+    $today = clean(date("Y-m-d"));
+        
+    $isZoneLeader = clean($_SESSION['role']) == clean('Zone Leader') ? true : false;
+    $zone_barangay = isset($_SESSION['barangay']) ? clean($_SESSION['barangay']) : '';
+    $all_barangay = [
+        "Kangwayan",
+        "Kodia",
+        "Pili",
+        "Bunakan",
+        "Tabagak",
+        "Maalat",
+        "Tarong",
+        "Malbago",
+        "Mancilang",
+        "Kaongkod",
+        "San Agustin",
+        "Poblacion",
+        "Tugas",
+        "Talangnan",
+    ];
+
+
+    $count_tblactivity = $con->query("SELECT * FROM tblactivity WHERE DATE(date_created) = '$today'");
+
+    $count_tblactivity = $con->prepare("SELECT * FROM tblactivity WHERE DATE(date_created) = ?");
+    $count_tblactivity->bind_param('s', $today);
+
+    var_dump($count_tblactivity);
+
+?>
