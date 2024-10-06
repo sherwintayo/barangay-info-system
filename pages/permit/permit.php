52f2c55154b0f43109677ba36ec465ae1c4198d4
four-J-MAX <cuevaj186@gmail.com>
2024-10-05 23:06:47 -0700

    commit

--- pages/permit/permit.php
+++ pages/permit/permit.php
@@ -35,8 +35,7 @@ if (!isset($_SESSION['role'])) {
     
         $today = clean(date("Y-m-d"));
             
-        $isZoneLeader = clean($_SESSION['role']) == clean('Zone Leader') ? true : false;
-        $zone_barangay = isset($_SESSION['barangay']) ? clean($_SESSION['barangay']) : '';
+       
         
         ?>
         <?php //include('../header.php'); ?>
