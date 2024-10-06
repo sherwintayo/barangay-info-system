44a96f11cdb6a544b8d3a51538c72fba0931e577
four-J-MAX <cuevaj186@gmail.com>
2024-10-05 22:28:59 -0700

    commit

--- pages/header.php
+++ pages/header.php
@@ -1,10 +1,5 @@
 
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
-<script src="./assets/js/jquery.min.js"></script>
-<script src="./assets/js/bootstrap.min.js"></script>
 
 <?php
 function clean($data): string{
