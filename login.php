bf96860dc263b945cf879b87b498419b5c33aeda
four-J-MAX <cuevaj186@gmail.com>
2024-10-26 22:55:09 -0700

    commig

--- login.php
+++ login.php
@@ -91,10 +91,10 @@ session_start();
                 <div class="panel-body">
                     <form role="form" method="post">
                         <?php 
-                                $stmt = $con->query("SELECT username FROM tbluser WHERE username= 'angel'");
-                                echo "<pre>";
-                                var_dump($stmt->fetch_all());
-                                echo "</pre>";
+                                // $stmt = $con->query("SELECT username FROM tbluser WHERE username= 'angel'");
+                                // echo "<pre>";
+                                // var_dump($stmt->fetch_all());
+                                // echo "</pre>";
                             
                         ?>
 
