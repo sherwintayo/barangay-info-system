52633622c754c55059fce0afa491ecf1cad1f31d
four-J-MAX <cuevaj186@gmail.com>
2024-11-23 00:26:50 +0800

    Update zone.php

--- pages/zone/zone.php
+++ pages/zone/zone.php
@@ -1,152 +1,115 @@
 <!DOCTYPE html>
 <html>
 
-    <?php
-    session_start();
-    if(!isset($_SESSION['role']))
-    {
-        header("Location: ../../login.php"); 
-    }
-    else
-    {
-    ob_start();
-    include('../head_css.php'); ?>
-    <body class="skin-black">
-        <!-- header logo: style can be found in header.less -->
-        <?php 
-        
-        include "../connection.php";
-        ?>
-        <?php include('../header.php'); ?>
+<?php
+session_start();
+if (!isset($_SESSION['role'])) {
+    header("Location: ../../login.php");
+    exit;
+}
+ob_start();
+include('../head_css.php'); ?>
+<body class="skin-black">
+    <?php include "../connection.php"; ?>
+    <?php include('../header.php'); ?>
 
-        <div class="wrapper row-offcanvas row-offcanvas-left">
-            <!-- Left side column. contains the logo and sidebar -->
-            <?php include('../sidebar-left.php'); ?>
+    <div class="wrapper row-offcanvas row-offcanvas-left">
+        <?php include('../sidebar-left.php'); ?>
 
-            <!-- Right side column. Contains the navbar and content of the page -->
-            <aside class="right-side">
-                <!-- Content Header (Page header) -->
-                <section class="content-header">
-                    <h1>
-                        Zone/Purok Leader
-                    </h1>
-                    
-                </section>
+        <aside class="right-side">
+            <section class="content-header">
+                <h1>Zone/Purok Leader</h1>
+            </section>
 
-                <!-- Main content -->
-                <section class="content">
-                    <div class="row">
-                        <!-- left column -->
-                            <div class="box">
-                                <div class="box-header">
-                                    <div style="padding:10px;">
-                                        
-                                        <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#addZoneModal"><i class="fa fa-user-plus" aria-hidden="true"></i> Add Zone/Purok Leader</button>  
-                                        <?php 
-                                            if(!isset($_SESSION['staff']))
-                                            {
-                                        ?>
-                                        <button class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteModal"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button> 
+            <section class="content">
+                <div class="row">
+                    <div class="box">
+                        <div class="box-header">
+                            <div style="padding:10px;">
+                                <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#addZoneModal"><i class="fa fa-user-plus" aria-hidden="true"></i> Add Zone/Purok Leader</button>
+                                <?php if (!isset($_SESSION['staff'])) { ?>
+                                    <button class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteModal"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
+                                <?php } ?>
+                            </div>
+                        </div>
+
+                        <div class="box-body table-responsive">
+                            <form method="post">
+                                <table id="table" class="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <?php if (!isset($_SESSION['staff'])) { ?>
+                                                <th style="width: 20px !important;"><input type="checkbox" class="cbxMain" onchange="checkMain(this)" /></th>
+                                            <?php } ?>
+                                            <th>Zone/Purok</th>
+                                            <th>Username</th>
+                                            <th style="width: 40px !important;">Option</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
                                         <?php
+                                        $query = "SELECT * FROM tblzone";
+                                        $squery = mysqli_query($con, $query);
+                                        while ($row = mysqli_fetch_array($squery)) {
+                                            $isApproved = $row['isApproved'] == 1;
+                                            echo '<tr>';
+                                            if (!isset($_SESSION['staff'])) {
+                                                echo '<td><input type="checkbox" name="chk_delete[]" class="chk_delete" value="' . $row['id'] . '" /></td>';
                                             }
+                                            echo '<td>' . $row['zone'] . '</td>';
+                                            echo '<td>' . $row['username'] . '</td>';
+                                            echo '<td>';
+                                            if ($isApproved) {
+                                                echo '<span class="badge badge-success">Approved</span>';
+                                            } else {
+                                                echo '<button class="btn btn-success btn-sm approve-btn" data-id="' . $row['id'] . '"><i class="fa fa-check" aria-hidden="true"></i> Approve</button>';
+                                            }
+                                            echo '</td>';
+                                            echo '</tr>';
+                                        }
                                         ?>
-                                
-                                    </div>                                
-                                </div><!-- /.box-header -->
-                                <div class="box-body table-responsive">
-                                <form method="post">
-                                    <table id="table" class="table table-bordered table-striped">
-                                        <thead>
-                                            <tr>
-                                                <?php 
-                                                if(!isset($_SESSION['staff']))
-                                                {
-                                                ?>
-                                                <th style="width: 20px !important;"><input type="checkbox" name="chk_delete[]" class="cbxMain" onchange="checkMain(this)"/></th>
-                                                <?php
-                                                    }
-                                                ?>
-                                                <th>Zone/Purok</th>
-                                                <th>Userame</th>
-                                                <th style="width: 40px !important;">Option</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php
-                                            if(!isset($_SESSION['staff'])){
-                                                if ($isZoneLeader) {
-                                                    $squery = mysqli_query($con, "select * from tblzone WHERE barangay = '$zone_barangay' ");
-                                                }else{
-                                                    $squery = mysqli_query($con, "select * from tblzone ");
-                                                }
-                                                while($row = mysqli_fetch_array($squery))
-                                                {
-                                                    echo '
-                                                    <tr>
-                                                        <td><input type="checkbox" name="chk_delete[]" class="chk_delete" value="'.$row['id'].'" /></td>
-                                                        <td>'.$row['zone'].'</td>
-                                                        <td>'.$row['username'].'</td>
-                                                        <td><button class="btn btn-primary btn-sm" data-target="#editModal'.$row['id'].'" data-toggle="modal"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></td>
-                                                    </tr>
-                                                    ';
+                                    </tbody>
+                                </table>
 
-                                                    include "edit_modal.php";
-                                                }
-                                            }
-                                            else{
-                                                if ($isZoneLeader) {
-                                                    $squery = mysqli_query($con, "select * from tblzone WHERE barangay = '$zone_barangay' ");
-                                                }else{
-                                                    $squery = mysqli_query($con, "select * from tblzone ");
-                                                }
-                                                while($row = mysqli_fetch_array($squery))
-                                                {
-                                                    echo '
-                                                    <tr>
-                                                        <td>'.$row['zone'].'</td>
-                                                        <td>'.$row['username'].'</td>
-                                                        <td><button class="btn btn-primary btn-sm" data-target="#editModal'.$row['id'].'" data-toggle="modal"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></td>
-                                                    </tr>
-                                                    ';
+                                <?php include "../deleteModal.php"; ?>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </aside>
+    </div>
 
-                                                    include "edit_modal.php";
-                                                }
-                                            }
-                                            ?>
-                                        </tbody>
-                                    </table>
-
-                                    <?php include "../deleteModal.php"; ?>
-
-                                    </form>
-                                </div><!-- /.box-body -->
-                            </div><!-- /.box -->
-
-                            <?php include "../edit_notif.php"; ?>
-
-                            <?php include "../added_notif.php"; ?>
-
-                            <?php include "../delete_notif.php"; ?>
-                            <?php include "../duplicate_error.php"; ?>
-
-            <?php include "add_modal.php"; ?>
-
-            <?php include "function.php"; ?>
-
-
-                    </div>   <!-- /.row -->
-                </section><!-- /.content -->
-            </aside><!-- /.right-side -->
-        </div><!-- ./wrapper -->
-        <!-- jQuery 2.0.2 -->
-        <?php }
-        include "../footer.php"; ?>
-<script type="text/javascript">
-    $(function() {
-        $("#table").dataTable({
-           "aoColumnDefs": [ { "bSortable": false, "aTargets": [ 0,3 ] } ],"aaSorting": []
+    <?php include "../footer.php"; ?>
+    <script type="text/javascript">
+        $(function() {
+            $("#table").dataTable({
+                "aoColumnDefs": [{
+                    "bSortable": false,
+                    "aTargets": [0, 3]
+                }],
+                "aaSorting": []
+            });
         });
-    });
-</script>
-    </body>
+
+        $(document).on('click', '.approve-btn', function() {
+            const id = $(this).data('id');
+            if (confirm('Are you sure you want to approve this zone?')) {
+                $.ajax({
+                    url: 'approve_zone.php',
+                    type: 'POST',
+                    data: { id: id },
+                    success: function(response) {
+                        if (response === 'success') {
+                            alert('Zone approved successfully!');
+                            location.reload(); // Reload the page to reflect the changes
+                        } else {
+                            alert('Failed to approve the zone.');
+                        }
+                    }
+                });
+            }
+        });
+    </script>
+</body>
 </html>
