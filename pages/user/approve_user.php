091d28a45d592b938b748ffc6ac558b88b04dff6
four-J-MAX <cuevaj186@gmail.com>
2024-11-23 11:58:41 +0800

    Create approve_user.php

---
+++ pages/user/approve_user.php
@@ -0,0 +1,20 @@
+<?php
+include "../connection.php";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['id'])) {
+    $id = intval($_POST['id']);
+    
+    $query = "UPDATE tbluser SET isApproved = 1 WHERE id = ?";
+    $stmt = $con->prepare($query);
+    $stmt->bind_param('i', $id);
+
+    if ($stmt->execute()) {
+        echo 'success';
+    } else {
+        echo 'error';
+    }
+
+    $stmt->close();
+    $con->close();
+}
+?>
