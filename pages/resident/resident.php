41e8c537dfe422b656496fbf1f664427bd02f6a7
four-J-MAX <cuevaj186@gmail.com>
2024-11-24 11:08:31 +0800

    Update resident.php

--- pages/resident/resident.php
+++ pages/resident/resident.php
@@ -1,7 +1,5 @@
 <!DOCTYPE html>
 <html>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
 <?php
 session_start();
