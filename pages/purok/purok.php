dedb533092bc176195eb520fa57228a3afe613d9
four-J-MAX <cuevaj186@gmail.com>
2024-11-25 16:19:27 +0800

    Update purok.php

--- pages/purok/purok.php
+++ pages/purok/purok.php
@@ -1,24 +1,21 @@
 <!DOCTYPE html>
 <html>
 
-<?php
-error_reporting(E_ALL);
-session_start();
-if (!isset($_SESSION['role'])) {
-    header("Location: ../../login.php");
-} else {
+    <?php
+    session_start();
+    if(!isset($_SESSION['role']))
+    {
+        header("Location: ../../login.php"); 
+    }
+    else
+    {
     ob_start();
-    include('../head_css.php');
-
-?>
-
+    include('../head_css.php'); ?>
     <body class="skin-black">
         <!-- header logo: style can be found in header.less -->
-        <?php
-
-        include "../connection.php";
+        <?php 
         
-
+        include "../connection.php";
         ?>
         <?php include('../header.php'); ?>
 
@@ -31,83 +28,90 @@ if (!isset($_SESSION['role'])) {
                 <!-- Content Header (Page header) -->
                 <section class="content-header">
                     <h1>
-                        Permit Clearance
+                        Purok
                     </h1>
-
+                    
                 </section>
 
                 <!-- Main content -->
                 <section class="content">
-                    <?php
-                    if (($_SESSION['role'] == "Administrator") || isset($_SESSION['staff'])) {
-                        require 'Administrator.php';
-                    } elseif ($_SESSION['role'] == "Zone Leader") {
-                        require 'ZoneLeader.php';
-                    }else{
-                        require 'Default.php';
-                    }
-                    ?>
-                </section>
+                    <div class="row">
+                        <!-- left column -->
+                            <div class="box">
+                                <div class="box-header">
+                                    <div style="padding:10px;">
+                                        
+                                        <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#addModal"><i class="fa fa-user-plus" aria-hidden="true"></i> Add Staff</button>  
+                                        <button class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteModal"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button> 
+                                    </div>                                
+                                </div><!-- /.box-header -->
+                                <div class="box-body table-responsive">
+                                <form method="post">
+                                    <table id="table" class="table table-bordered table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th style="width: 20px !important;"><input type="checkbox" name="chk_delete[]" class="cbxMain" onchange="checkMain(this)"/></th>
+                                                <th>Name</th>
+                                                <th>Username</th>
+                                                <th style="width: 40px !important;">Option</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            if ($isZoneLeader) {
+                                                $squery = mysqli_query($con, "select * from tblstaff WHERE barangay = '$zone_barangay'");
+                                            }else{
+                                                $squery = mysqli_query($con, "select * from tblstaff");
+                                            }
+                                            while($row = mysqli_fetch_array($squery))
+                                            {
+                                                echo '
+                                                <tr>
+                                                    <td><input type="checkbox" name="chk_delete[]" class="chk_delete" value="'.$row['id'].'" /></td>
+                                                    <td>'.$row['name'].'</td>
+                                                    <td>'.$row['username'].'</td>
+                                                    <td><button class="btn btn-primary btn-sm" data-target="#editModal'.$row['id'].'" data-toggle="modal"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></td>
+                                                </tr>
+                                                ';
+
+                                                include "edit_modal.php";
+                                            }
+                                            ?>
+                                        </tbody>
+                                    </table>
+
+                                    <?php include "../deleteModal.php"; ?>
+
+                                    </form>
+                                </div><!-- /.box-body -->
+                            </div><!-- /.box -->
+
+                            <?php include "../edit_notif.php"; ?>
+
+                            <?php include "../added_notif.php"; ?>
+
+                            <?php include "../delete_notif.php"; ?>
+
+                            <?php include "../duplicate_error.php"; ?>
+
+            <?php include "add_modal.php"; ?>
+
+            <?php include "function.php"; ?>
+
+
+                    </div>   <!-- /.row -->
+                </section><!-- /.content -->
             </aside><!-- /.right-side -->
         </div><!-- ./wrapper -->
         <!-- jQuery 2.0.2 -->
-
-    <?php
-
-    include "../edit_notif.php";
-    include "../added_notif.php";
-    include "../delete_notif.php";
-    include "../duplicate_error.php";
-    include "add_modal.php";
-    include "function.php";
-}
-include "../footer.php"; ?>
-    <script type="text/javascript">
-        <?php
-        if (!isset($_SESSION['staff'])) {
-        ?>
-            $(function() {
-                $("#table").dataTable({
-                    "aoColumnDefs": [{
-                        "bSortable": false,
-                        "aTargets": [0, 7]
-                    }],
-                    "aaSorting": []
-                });
-                $("#table1").dataTable({
-                    "aoColumnDefs": [{
-                        "bSortable": false,
-                        "aTargets": [0, 4]
-                    }],
-                    "aaSorting": []
-                });
-                $(".se lect2").select2();
-            });
-        <?php
-        } else {
-        ?>
-            $(function() {
-                $("#table").dataTable({
-                    "aoColumnDefs": [{
-                        "bSortable": false,
-                        "aTargets": [6]
-                    }],
-                    "aaSorting": []
-                });
-
-                $("#table1").dataTable({
-                    "aoColumnDefs": [{
-                        "bSortable": false,
-                        "aTargets": [3]
-                    }],
-                    "aaSorting": []
-                });
-                $(".select2").select2();
-            });
-        <?php
-        }
-        ?>
-    </script>
+        <?php }
+        include "../footer.php"; ?>
+<script type="text/javascript">
+    $(function() {
+        $("#table").dataTable({
+           "aoColumnDefs": [ { "bSortable": false, "aTargets": [ 0,3 ] } ],"aaSorting": []
+        });
+    });
+</script>
     </body>
-
 </html>
