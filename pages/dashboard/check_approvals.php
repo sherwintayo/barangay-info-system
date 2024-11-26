bf904313763550afeea65552f8a230d4445c35c1
four-J-MAX <cuevaj186@gmail.com>
2024-11-26 20:37:27 +0800

    Create check_approvals.php

---
+++ pages/dashboard/check_approvals.php
@@ -0,0 +1,15 @@
+<?php
+// Include database connection
+include "pages/connection.php";
+
+// Query to check for pending approvals
+$query = "SELECT COUNT(*) as pending_count FROM tbluser WHERE type = 'Zone Leader' AND isApproved = 0";
+$result = mysqli_query($con, $query);
+
+// Fetch result
+$data = mysqli_fetch_assoc($result);
+$pendingCount = $data['pending_count'];
+
+// Return JSON response
+echo json_encode(['pendingCount' => $pendingCount]);
+?>
