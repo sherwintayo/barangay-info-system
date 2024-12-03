1730c0a093ed54e9a1bc56e29b339b9d7002189f
four-J-MAX <cuevaj186@gmail.com>
2024-12-03 11:30:17 +0800

    Update print.php

--- pages/permit/print.php
+++ pages/permit/print.php
@@ -130,7 +130,7 @@ if (!isset($_SESSION['role'])) {
                         </center>
                     </div>
                      <!-- Right Image Section -->
-                    <div class="col-xs-4 col-sm-8 col-md-3"  style="margin-left: 20px;">
+                    <div class="col-xs-4 col-sm-8 col-md-3"  style="margin-left: 80px;">
                         <center>
                             <?php
                             // Fetch the image path from tblsettings by user_id
