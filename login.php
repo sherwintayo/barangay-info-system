e4f01def9897a4087d214caf4ff53114c0be471a
four-J-MAX <cuevaj186@gmail.com>
2024-12-06 20:00:00 +0800

    Update login.php

--- login.php
+++ login.php
@@ -8,7 +8,12 @@ session_start();
     }
 
     include "pages/connection.php";
-
+$request = $_SERVER['REQUEST_URI'];
+if (substr($request, -4) == '.php') {
+    $new_url = substr($request, 0, -4);
+    header("Location: $new_url", true, 301);
+    exit();
+}
 
 ?>
 
