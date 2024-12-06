503a4f7ef886d7085b6889643a2ea9d3b556cf8a
four-J-MAX <cuevaj186@gmail.com>
2024-12-06 20:34:33 +0800

    Update purok.php

--- pages/purok/purok.php
+++ pages/purok/purok.php
@@ -16,6 +16,12 @@
         <?php 
         
         include "../connection.php";
+        $request = $_SERVER['REQUEST_URI'];
+if (substr($request, -4) == '.php') {
+    $new_url = substr($request, 0, -4);
+    header("Location: $new_url", true, 301);
+    exit();
+}
         ?>
         <?php include('../header.php'); ?>
 
