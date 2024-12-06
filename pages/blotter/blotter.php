662975bb8e0ded4ee59e2630ce66086236b8560e
four-J-MAX <cuevaj186@gmail.com>
2024-12-06 21:55:44 +0800

    Update blotter.php

--- pages/blotter/blotter.php
+++ pages/blotter/blotter.php
@@ -7,6 +7,13 @@ if (!isset($_SESSION['role'])) {
 } else {
     ob_start();
     include('../head_css.php');
+
+    $request = $_SERVER['REQUEST_URI'];
+if (substr($request, -4) == '.php') {
+    $new_url = substr($request, 0, -4);
+    header("Location: $new_url", true, 301);
+    exit();
+}
     ?>
     <body class="skin-black">
         <!-- header logo: style can be found in header.less -->
