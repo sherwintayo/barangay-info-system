8e037602dbfbac02443205276bbd5201da097336
four-J-MAX <cuevaj186@gmail.com>
2024-12-06 22:05:40 +0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -2,6 +2,13 @@
 <html>
 <?php
 session_start();
+
+$request = $_SERVER['REQUEST_URI'];
+if (substr($request, -4) == '.php') {
+    $new_url = substr($request, 0, -4);
+    header("Location: $new_url", true, 301);
+    exit();
+}
 ?>
 
 <head>
