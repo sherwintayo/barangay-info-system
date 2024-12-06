e5eb9b8a0183506d265b1cbc79f33cf8326fbd84
four-J-MAX <cuevaj186@gmail.com>
2024-12-06 20:03:32 +0800

    Update officials.php

--- pages/officials/officials.php
+++ pages/officials/officials.php
@@ -197,6 +197,13 @@ if (!isset($_SESSION['staff'])) {
         });
     <?php
     }
+
+$request = $_SERVER['REQUEST_URI'];
+if (substr($request, -4) == '.php') {
+    $new_url = substr($request, 0, -4);
+    header("Location: $new_url", true, 301);
+    exit();
+}
     ?>
 </script>
     </body>
