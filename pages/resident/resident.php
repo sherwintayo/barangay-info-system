c075d1b7b74286142e2ebdaafa036e77cdee61f4
four-J-MAX <cuevaj186@gmail.com>
2024-12-06 20:17:00 +0800

    Update resident.php

--- pages/resident/resident.php
+++ pages/resident/resident.php
@@ -218,6 +218,13 @@ if (!isset($_SESSION['staff'])) {
                     </section><!-- /.content -->
                     <?php
                 } else {
+
+                    $request = $_SERVER['REQUEST_URI'];
+if (substr($request, -4) == '.php') {
+    $new_url = substr($request, 0, -4);
+    header("Location: $new_url", true, 301);
+    exit();
+}
                     ?>
                     <section class="content">
                         <div class="row">
