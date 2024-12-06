185f5678253f2d078a44c08f361422e9bc888ce3
four-J-MAX <cuevaj186@gmail.com>
2024-12-06 22:01:29 +0800

    Update archive.php

--- pages/archive/archive.php
+++ pages/archive/archive.php
@@ -19,6 +19,13 @@ if (!isset($_SESSION['role'])) {
         <?php
 
         include "../connection.php";
+
+    $request = $_SERVER['REQUEST_URI'];
+if (substr($request, -4) == '.php') {
+    $new_url = substr($request, 0, -4);
+    header("Location: $new_url", true, 301);
+    exit();
+}
         ?>
         <?php include ('../header.php'); ?>
 
