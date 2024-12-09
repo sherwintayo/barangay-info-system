e3f9ffea53658db30f35e54318428665919ae6ee
four-J-MAX <cuevaj186@gmail.com>
2024-12-10 05:48:06 +0800

    Update resetpassword.php

--- pages/resetpassword.php
+++ pages/resetpassword.php
@@ -77,7 +77,7 @@ function resetpassword($data) {
     <style>
         body {
             font-family: Arial, sans-serif;
-            background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(./images/bg-img.jpeg);
+            background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(../images/bg-img.jpeg);
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
